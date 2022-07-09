d93dd6eeff61fe6b0de4ba6997b9dc6a8cb4a758
bilgramiraza <bilgramiraza@yahoo.com>
2022-07-10 01:17:03 +0530

    Added Bootstrap to project

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='container'>
+      <Form>
+        <Form.Group as={Row} className="mb-3" controlId="userName">
+          <Form.Label column sm="2">
+            User Name
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="text" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="pwd">
+          <Form.Label column sm="2">
+            Password
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+      </Form>
+    </div>
   )
 }
 
