0849d04c0b2494cd6b48a8abd0bb6be4f1e25dd5
bilgramiraza <bilgramiraza@yahoo.com>
2022-07-10 14:01:44 +0530

    Added New Route for password setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './components/Login.js';
 import Register from './components/Register.js';
 import Dashboard from './components/Dashboard.js';
+import Setpassword from "./components/Setpassword";
 
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Setpassword" element={<Setpassword />} />
       </Routes>
     </BrowserRouter>
   );
