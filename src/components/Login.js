cbe9dcccb02d12d06f18129229f324386dd0edf0
bilgramiraza <bilgramiraza@yahoo.com>
2022-07-10 14:02:53 +0530

    Fixed Bootstrap implementation and added link to register Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div className='container'>
-      <Form>
+    <Container>
+      <Form className='mx-auto'>
         <Form.Group as={Row} className="mb-3" controlId="userName">
           <Form.Label column sm="2">
             User Name
@@ -24,8 +27,10 @@ function Login() {
             <Form.Control type="password" placeholder="Password" />
           </Col>
         </Form.Group>
+        <Button variant="primary">Login</Button>
       </Form>
-    </div>
+      <Link to="/register">Don't have an account? Register Here</Link>
+    </Container>
   )
 }
 
