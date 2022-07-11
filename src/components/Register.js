93c43ab2176371be4b5b44063b088d45127f8790
bilgramiraza <bilgramiraza@yahoo.com>
2022-07-11 15:32:45 +0530

    Setup internal State management for the form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,44 +1,100 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from "react-router-dom";
-import { useSelector } from 'react-redux';
 
 function Register() {
   let navigate = useNavigate();
+
+  const getUserInfo = (e) =>{
+    if(email&&firstName&&lastName&&mobileNo&&tandc){
+      console.log('email:'+email);
+      console.log('FullName:'+firstName+' '+lastName);
+      console.log('Mobile no:'+mobileNo);
+      console.log('Terms and conditions:'+tandc);
+      setEmail('');
+      setFirstName('');
+      setLastName('');
+      setMobileNo('');
+      setTandC(false);
+    }
+  }
   const setPassword = (e) =>{
     navigate('/otp');
   }
-  console.log(useSelector((store)=>{console.log(store)}));
+  const [email,setEmail] = useState('');
+  const [firstName,setFirstName] = useState('');
+  const [lastName,setLastName] = useState('');
+  const [mobileNo,setMobileNo] = useState(0);
+  const [tandc, setTandC] = useState(false);
+
+  const handleEmailChange = (e) =>{setEmail(e.target.value)};
+  const handleFirstNameChange = (e) =>{setFirstName(e.target.value)};
+  const handleLastNameChange = (e) =>{setLastName(e.target.value)};
+  const handleMobileNoChange = (e) =>{setMobileNo(e.target.value)};
+  const handleTandCChange = (e) =>{setTandC(!tandc)};
+
   return (
     <Container>
-      <Form onSubmit={setPassword}>
+      <Form>
         <Form.Group className="mb-3" controlId="basicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control 
+            type="email" 
+            placeholder="Enter email" 
+            value={email} 
+            onChange={handleEmailChange}
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
+
         <InputGroup className="mb-3">
           <InputGroup.Text>First and Last name</InputGroup.Text>
-          <Form.Control aria-label="First name" placeholder="First Name"/>
-          <Form.Control aria-label="Last name" placeholder="Last Name"/>
+          <Form.Control 
+            aria-label="First name" 
+            placeholder="First Name"
+            value={firstName}
+            onChange={handleFirstNameChange}
+            required
+          />
+          <Form.Control 
+            aria-label="Last name" 
+            placeholder="Last Name"
+            value={lastName}
+            onChange={handleLastNameChange}
+            required
+          />
         </InputGroup>
+
         <Form.Group className="mb-3" controlId="basicMobileNo">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="number" placeholder="123456789" />
+          <Form.Label>Mobile Number</Form.Label>
+          <Form.Control 
+            type="number" 
+            placeholder="123456789" 
+            value={mobileNo}
+            onChange={handleMobileNoChange}
+            required
+          />
         </Form.Group>
-        <Form.Group controlId="formFileSm" className="mb-3">
-          <Form.Label>Upload an Image</Form.Label>
-          <Form.Control type="file"/>
-        </Form.Group>
+
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="I agree to the Terms and Conditions" />
+          <Form.Check 
+            type="checkbox" 
+            label="I agree to the Terms and Conditions" 
+            checked={tandc}
+            onChange={handleTandCChange}
+            required
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+
+        <Button variant="primary" type="button" onClick={getUserInfo}>
           Submit
         </Button>
       </Form>
